b2b0fdd9c70deec3df5af6043c4e43e7c9cfac7d
lekishonsheila <lekishonsheila@gmail.com>
2023-03-09 08:53:44 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,63 @@
+fun main() {
+
+    println()
+    addition("24","30")
+    println()
+    subtraction("40","20")
+    println()
+    division("60","2")
+    println()
+    multiplication("30","4")
+    println()
+
+
+
+
+
+
+
+}
+
+fun name(num1:String, num2:String):String {
+    return(num1)
+
+
+
+}
+
+
+
+fun join(num1:String,num2:String):String{
+    var num1="Hello"
+    var num2="my name is sheila"
+    return("Hello $num2")
+
+}
+
+
+
+
+
+
+fun addition(num1: String,num2: String){
+    var num1=10
+    var num2=30
+    println(num1+num2)
+
+}
+fun subtraction(num3:String,num4:String){
+    var num3=40
+    var num4=20
+    println(num3-num4)
+}
+fun division(num5:String,num6:String){
+    var num5=60
+    var num6=2
+    println(num5/num6)
+}
+fun multiplication(num7:String,num8:String){
+    var num7=30
+    var num8=4
+    println(num7*num8)
+}
+
